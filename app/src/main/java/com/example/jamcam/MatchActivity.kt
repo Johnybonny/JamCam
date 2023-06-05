4854653e087755b938b4302cc55321c419fbe1e1
Johnybonny <szympast@gmail.com>
2023-06-05 23:35:58 +0200

    Modify permissions requests

--- app/src/main/java/com/example/jamcam/MatchActivity.kt
+++ app/src/main/java/com/example/jamcam/MatchActivity.kt
@@ -1,8 +1,11 @@
 package com.example.jamcam
 
 import android.Manifest
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -13,24 +16,21 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MatchActivity : AppCompatActivity() {
 
-    private val MICROPHONE_PERMISSION_CODE = 201
-    private val CAMERA_PERMISSION_CODE = 202
-    private val WRITE_EXTERNAL_STORAGE_CODE = 203
-    private val READ_EXTERNAL_STORAGE_CODE = 204
+    private val MULTIPLE_PERMISSIONS_CODE = 200
+
+    val permissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
     private val SYSTEM_ALERT_WINDOW_CODE = 205
-    private val FOREGROUND_SERVICE_CODE = 206
-
-
-    private var isPermissionsGrantedMicrophone = false
-    private var isPermissionsGrantedCamera = false
-    private var isPermissionsGrantedStorageRead = false
-    private var isPermissionsGrantedStorageWrite = false
-    private var isPermissionsGrantedAlert = false
-    private var isPermissionsGrantedForeground = false
 
     private var ableToRecord = false
     private var isRecording = false
@@ -53,16 +53,39 @@ class MatchActivity : AppCompatActivity() {
 
         val timeView: TextView = findViewById(R.id.time_view)
 
-        getMicrophonePermission()
-        getCameraPermission()
-        getReadExternalStoragePermission()
-        getWriteExternalStoragePermission()
-        getAlertWindowPermission()
-        getForegroundServicePermission()
+
+        if (!checkPermissionForSystemAlertWindow()) return
+
+        if (checkPermissions()) {
+            startApp()
+        }
 
         startButton.setOnClickListener { startRecording(timeView) }
         stopButton.setOnClickListener { stopRecording() }
         clickButton.setOnClickListener { reportHighlight() }
+    }
+
+    private fun startApp() {
+        ableToRecord = true
+    }
+
+    private fun checkPermissions(): Boolean {
+        val listPermissionsNeeded = ArrayList<String>()
+        for (p in permissions) {
+            val result = ContextCompat.checkSelfPermission(this, p)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p)
+            }
+        }
+        if (listPermissionsNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionsNeeded.toTypedArray(),
+                MULTIPLE_PERMISSIONS_CODE
+            )
+            return false
+        }
+        return true
     }
 
     private fun reportHighlight() {
@@ -100,18 +123,6 @@ class MatchActivity : AppCompatActivity() {
         stopService(intent)
     }
 
-    private fun checkCreateRecorder() {
-        if (isPermissionsGrantedMicrophone &&
-            isPermissionsGrantedCamera &&
-            isPermissionsGrantedStorageRead &&
-            isPermissionsGrantedStorageWrite &&
-            isPermissionsGrantedAlert &&
-            isPermissionsGrantedForeground
-        ) {
-            ableToRecord = true
-        }
-    }
-
     private fun startRecording(timeView: TextView) {
         if (!isRecording && ableToRecord) {
             runTimer(timeView)
@@ -130,7 +141,7 @@ class MatchActivity : AppCompatActivity() {
 
     private fun createHighlights() {
         val videoEditor = VideoEditor("JamCam", "original.mp4")
-        for(startTime in timestamps) {
+        for (startTime in timestamps) {
             val start = UtilityClass.secondsToTimestamp(startTime)
             val stop = UtilityClass.addTime(start, highlightLength)
             videoEditor.createHighlight(this, start, stop)
@@ -138,151 +149,54 @@ class MatchActivity : AppCompatActivity() {
 
     }
 
-
-    private fun getMicrophonePermission() {
-        println("getMicrophonePermission")
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-            == PackageManager.PERMISSION_DENIED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.RECORD_AUDIO),
-                MICROPHONE_PERMISSION_CODE
-            )
-        } else {
-            isPermissionsGrantedMicrophone = true
-            checkCreateRecorder()
-        }
-    }
-
-    private fun getCameraPermission() {
-        println("getCameraPermission")
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            == PackageManager.PERMISSION_DENIED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.CAMERA),
-                CAMERA_PERMISSION_CODE
-            )
-        } else {
-            isPermissionsGrantedCamera = true
-            checkCreateRecorder()
-        }
-    }
-
-    private fun getWriteExternalStoragePermission() {
-        println("getWriteExternalStoragePermission")
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            == PackageManager.PERMISSION_DENIED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                WRITE_EXTERNAL_STORAGE_CODE
-            )
-        } else {
-            isPermissionsGrantedStorageWrite = true
-            checkCreateRecorder()
-        }
-    }
-
-    private fun getReadExternalStoragePermission() {
-        println("getExternalStoragePermission")
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-            == PackageManager.PERMISSION_DENIED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                READ_EXTERNAL_STORAGE_CODE
-            )
-        } else {
-            isPermissionsGrantedStorageRead = true
-            checkCreateRecorder()
-        }
-    }
-
-    private fun getForegroundServicePermission() {
-        println("getForegroundServicePermission")
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE)
-            == PackageManager.PERMISSION_DENIED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                ActivityCompat.requestPermissions(
-                    this, arrayOf(Manifest.permission.FOREGROUND_SERVICE),
-                    FOREGROUND_SERVICE_CODE
-                )
+    private fun checkPermissionForSystemAlertWindow(): Boolean {
+        if (!isPermissionForSystemAlertWindowGranted()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                intent.data = Uri.parse("package:$packageName")
+                startActivityForResult(intent, SYSTEM_ALERT_WINDOW_CODE)
             }
+            return false
+        }
+        return true
+    }
+
+    private fun isPermissionForSystemAlertWindowGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.canDrawOverlays(this)
         } else {
-            isPermissionsGrantedForeground = true
-            checkCreateRecorder()
+            true
         }
     }
+
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun getAlertWindowPermission() {
-        println("getAlertWindowPermission")
-        if (!Settings.canDrawOverlays(this)) {
-            // Permission not granted, request it using the intent
-            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            startActivityForResult(intent, SYSTEM_ALERT_WINDOW_CODE)
-        } else {
-            // Permission already granted
-            isPermissionsGrantedAlert = true
-            checkCreateRecorder()
-        }
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == MICROPHONE_PERMISSION_CODE ||
-            requestCode == CAMERA_PERMISSION_CODE ||
-            requestCode == WRITE_EXTERNAL_STORAGE_CODE ||
-            requestCode == READ_EXTERNAL_STORAGE_CODE ||
-            requestCode == SYSTEM_ALERT_WINDOW_CODE ||
-            requestCode == FOREGROUND_SERVICE_CODE
-        ) {
-            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                when (requestCode) {
-                    MICROPHONE_PERMISSION_CODE -> {
-                        isPermissionsGrantedMicrophone = true
-                        checkCreateRecorder()
-                    }
-
-                    CAMERA_PERMISSION_CODE -> {
-                        isPermissionsGrantedCamera = true
-                        checkCreateRecorder()
-                    }
-
-                    WRITE_EXTERNAL_STORAGE_CODE -> {
-                        isPermissionsGrantedStorageWrite = true
-                        checkCreateRecorder()
-                    }
-
-                    READ_EXTERNAL_STORAGE_CODE -> {
-                        isPermissionsGrantedStorageRead = true
-                        checkCreateRecorder()
-                    }
-
-                    SYSTEM_ALERT_WINDOW_CODE -> {
-                        isPermissionsGrantedAlert = true
-                        checkCreateRecorder()
-                    }
-
-                    FOREGROUND_SERVICE_CODE -> {
-                        isPermissionsGrantedForeground = true
-                        checkCreateRecorder()
-                    }
-
-                    else -> {
-                        println("There is a problem in MainActivity -> onRequestPermissionsResult")
-                    }
+        when (requestCode) {
+            MULTIPLE_PERMISSIONS_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permissions granted
+                    startApp()
+                } else {
+                    // permissions not granted
+                    Toast.makeText(
+                        this,
+                        "Permissions denied. The app cannot start.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Toast.makeText(
+                        this,
+                        "Please re-start Open Dash Cam app and grant the requested permissions.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    finish()
                 }
-
-            } else {
-                // Permission denied, handle accordingly
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                return
             }
         }
     }
