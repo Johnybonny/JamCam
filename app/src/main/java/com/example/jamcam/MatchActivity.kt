394b60d68526913c1fea810140f4decc15642816
Johnybonny <szympast@gmail.com>
2023-06-06 12:27:05 +0200

    Cleanup match activity

--- app/src/main/java/com/example/jamcam/MatchActivity.kt
+++ app/src/main/java/com/example/jamcam/MatchActivity.kt
@@ -1,38 +1,16 @@
 package com.example.jamcam
 
-import android.Manifest
-import android.app.NotificationManager
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.provider.Settings
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
 
 class MatchActivity : AppCompatActivity() {
 
-//    private val MULTIPLE_PERMISSIONS_CODE = 200
-//
-//    val permissions = arrayOf(
-//        Manifest.permission.CAMERA,
-//        Manifest.permission.RECORD_AUDIO,
-//        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//        Manifest.permission.READ_EXTERNAL_STORAGE
-//    )
-//
-//    private val SYSTEM_ALERT_WINDOW_CODE = 205
-//
-//    private var ableToRecord = false
     private var isRecording = false
 
     private var seconds = 0
@@ -54,39 +32,12 @@ class MatchActivity : AppCompatActivity() {
         val timeView: TextView = findViewById(R.id.time_view)
 
 
-//        if (!checkPermissionForSystemAlertWindow()) return
-//
-//        if (checkPermissions()) {
-//            startApp()
-//        }
-
         startButton.setOnClickListener { startRecording(timeView) }
         stopButton.setOnClickListener { stopRecording() }
         clickButton.setOnClickListener { reportHighlight() }
     }
 
-//    private fun startApp() {
-//        ableToRecord = true
-//    }
 
-//    private fun checkPermissions(): Boolean {
-//        val listPermissionsNeeded = ArrayList<String>()
-//        for (p in permissions) {
-//            val result = ContextCompat.checkSelfPermission(this, p)
-//            if (result != PackageManager.PERMISSION_GRANTED) {
-//                listPermissionsNeeded.add(p)
-//            }
-//        }
-//        if (listPermissionsNeeded.isNotEmpty()) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                listPermissionsNeeded.toTypedArray(),
-//                MULTIPLE_PERMISSIONS_CODE
-//            )
-//            return false
-//        }
-//        return true
-//    }
 
     private fun reportHighlight() {
         val now = UtilityClass.now()
@@ -149,56 +100,6 @@ class MatchActivity : AppCompatActivity() {
 
     }
 
-//    private fun checkPermissionForSystemAlertWindow(): Boolean {
-//        if (!isPermissionForSystemAlertWindowGranted()) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-//                intent.data = Uri.parse("package:$packageName")
-//                startActivityForResult(intent, SYSTEM_ALERT_WINDOW_CODE)
-//            }
-//            return false
-//        }
-//        return true
-//    }
-//
-//    private fun isPermissionForSystemAlertWindowGranted(): Boolean {
-//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            Settings.canDrawOverlays(this)
-//        } else {
-//            true
-//        }
-//    }
 
-
-//    @RequiresApi(Build.VERSION_CODES.M)
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        when (requestCode) {
-//            MULTIPLE_PERMISSIONS_CODE -> {
-//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    // permissions granted
-//                    startApp()
-//                } else {
-//                    // permissions not granted
-//                    Toast.makeText(
-//                        this,
-//                        "Permissions denied. The app cannot start.",
-//                        Toast.LENGTH_LONG
-//                    ).show()
-//                    Toast.makeText(
-//                        this,
-//                        "Please re-start Open Dash Cam app and grant the requested permissions.",
-//                        Toast.LENGTH_LONG
-//                    ).show()
-//                    finish()
-//                }
-//                return
-//            }
-//        }
-//    }
 
 }
