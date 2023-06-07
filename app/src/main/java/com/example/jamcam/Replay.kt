db931db99944f7cbbbd6b0e87b64fde8525eb243
anras5 <anras1filip@gmail.com>
2023-06-07 14:12:04 +0200

    Migrate to ExoPlayer

--- app/src/main/java/com/example/jamcam/Replay.kt
+++
@@ -1,7 +0,0 @@
-package com.example.jamcam
-
-data class Replay(
-    var videoPath: String,
-    var videoTitle: String,
-    var videoDescription: String,
-)
--- app/src/main/java/com/example/jamcam/ReplaysActivity.kt
+++ app/src/main/java/com/example/jamcam/ReplaysActivity.kt
@@ -3,33 +3,92 @@ package com.example.jamcam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.example.jamcam.databinding.ActivityReplaysBinding
 import java.io.File
 
 class ReplaysActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReplaysBinding
+    private lateinit var adapter: VideoAdapter
+    private val videos = ArrayList<Video>()
+    private val exoPlayerItems = ArrayList<ExoPlayerItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_replays)
-
-        val viewPagerReplays: ViewPager2 = findViewById(R.id.viewPagerReplays)
+        binding = ActivityReplaysBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val path = "${applicationContext.filesDir.path}/replays"
-        val replays = mutableListOf<Replay>()
 
         val replayDirectory = File(path)
         if (replayDirectory.exists() && replayDirectory.isDirectory) {
             replayDirectory.listFiles()?.let { files ->
                 for (file in files) {
-                    val replay = Replay(
-                        file.absolutePath,
+                    val video = Video(
                         file.name,
-                        "Short video" // You can customize this description as needed
+                        "Short description",
+                        file.absolutePath,
                     )
-                    replays.add(replay)
+                    videos.add(video)
                 }
             }
         }
 
-        viewPagerReplays.adapter = ReplaysAdapter(replays)
+        adapter = VideoAdapter(this, videos, object : VideoAdapter.OnVideoPreparedListener {
+            override fun onVideoPrepared(exoPlayerItem: ExoPlayerItem) {
+                exoPlayerItems.add(exoPlayerItem)
+            }
+        })
 
+        binding.viewPager2.adapter = adapter
+
+        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                val previousIndex = exoPlayerItems.indexOfFirst { it.exoPlayer.isPlaying }
+                if (previousIndex != -1) {
+                    val player = exoPlayerItems[previousIndex].exoPlayer
+                    player.pause()
+                    player.playWhenReady = false
+                }
+                val newIndex = exoPlayerItems.indexOfFirst { it.position == position }
+                if (newIndex != -1) {
+                    val player = exoPlayerItems[newIndex].exoPlayer
+                    player.playWhenReady = true
+                    player.play()
+                }
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val index = exoPlayerItems.indexOfFirst { it.position == binding.viewPager2.currentItem }
+        if (index != -1) {
+            val player = exoPlayerItems[index].exoPlayer
+            player.pause()
+            player.playWhenReady = false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val index = exoPlayerItems.indexOfFirst { it.position == binding.viewPager2.currentItem }
+        if (index != -1) {
+            val player = exoPlayerItems[index].exoPlayer
+            player.playWhenReady = true
+            player.play()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (exoPlayerItems.isNotEmpty()) {
+            for (item in exoPlayerItems) {
+                val player = item.exoPlayer
+                player.stop()
+                player.clearMediaItems()
+            }
+        }
     }
 }
