7a41fa085dc4a0141f510630f7aa8e6b3a9808c6
anras5 <anras1filip@gmail.com>
2023-06-11 21:21:24 +0200

    Add permissions and Google API

--- app/src/main/java/com/example/jamcam/MainActivity.kt
+++ app/src/main/java/com/example/jamcam/MainActivity.kt
@@ -24,7 +24,9 @@ class MainActivity : AppCompatActivity() {
         Manifest.permission.CAMERA,
         Manifest.permission.RECORD_AUDIO,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
     )
     private val SYSTEM_ALERT_WINDOW_CODE = 205
     private var permissionsGranted = false
